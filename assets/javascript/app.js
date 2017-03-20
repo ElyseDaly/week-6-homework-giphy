952e4a6674f30de43a6c955f53bc1477180f16ed
ElyseDaly <elysedaly10@gmail.com>
2017-03-20 17:30:09 -0500

    fix to http vs https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,7 +43,7 @@ resetButtons();
 //GIFS
 function showGifs(){
     var action = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
     
     $.ajax({
         url: queryURL,
